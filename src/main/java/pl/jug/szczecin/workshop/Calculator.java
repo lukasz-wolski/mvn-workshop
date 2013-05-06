d88d1d8da94d227655f146322a4189c5d8d48ccc
Paweł Stawicki <pawelstawicki@gmail.com>
2013-05-06 23:51:37 +0200

    Kalkulator z testem

---
+++ src/main/java/pl/jug/szczecin/workshop/Calculator.java
@@ -0,0 +1,9 @@
+package pl.jug.szczecin.workshop;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ src/test/java/pl/jug/szczecin/workshop/CalculatorTest.java
@@ -0,0 +1,15 @@
+package pl.jug.szczecin.workshop;
+
+import org.testng.annotations.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class CalculatorTest {
+
+    @Test
+    public void shouldAddNumbers() {
+        int result = new Calculator().add(4, 6);
+
+        assertThat(result).isEqualTo(10);
+    }
+}
