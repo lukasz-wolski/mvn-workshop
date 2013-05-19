1d97ddd8b8907a9675207c9f54e63b52f2c1abd7
Paweł Stawicki <pawelstawicki@gmail.com>
2013-05-19 17:08:20 +0200

    UserStorageRest added

---
+++ webreg/src/main/java/pl/jug/szczecin/workshop/UserStorageRest.java
@@ -0,0 +1,16 @@
+package pl.jug.szczecin.workshop;
+
+import pl.jug.szczecin.workshop.entity.User;
+
+import java.util.List;
+
+public class UserStorageRest {
+
+    public void storeUser(String name, String surname, String email, Object webpage) {
+        //To change body of created methods use File | Settings | File Templates.
+    }
+
+    public List<User> getUsers() {
+        return null;
+    }
+}
