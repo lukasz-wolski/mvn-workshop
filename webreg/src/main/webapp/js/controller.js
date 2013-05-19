c1b50e68927e8e8add0353df3e2bd8559845ece2
Paweł Stawicki <pawelstawicki@gmail.com>
2013-05-19 17:08:20 +0200

    Users list added to the page

--- webreg/src/main/webapp/js/controller.js
+++ webreg/src/main/webapp/js/controller.js
@@ -1,5 +1,20 @@
 function UserRegController($scope) {
 
+    $scope.users = [
+        {
+            name: "Paweł",
+            surname: "Stawicki",
+            email: "pawelstawicki@gmail.com",
+            webpage: "pawelstawicki.blogspot.com"
+        },
+        {
+            name: "Leszek",
+            surname: "Gruchała",
+            email: "leszek@gmail.com",
+            webpage: "leszek.blogspot.com"
+        }
+    ];
+
     $scope.registerUser = function() {
         alert($scope.name + " " + $scope.email)
     }
