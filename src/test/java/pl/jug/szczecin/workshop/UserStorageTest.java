42ec88dc37e7314cf8936dc095e14e6b46028307
Paweł Stawicki <pawelstawicki@gmail.com>
2013-05-20 22:16:02 +0200

    Added another test to UserStorageTest

--- src/test/java/pl/jug/szczecin/workshop/UserStorageTest.java
+++ src/test/java/pl/jug/szczecin/workshop/UserStorageTest.java
@@ -13,6 +13,9 @@ public class UserStorageTest {
     public static final String SURENAME = "Stawicki";
     public static final String EMAIL = "pawelstawicki@gmail.com";
     public static final String WEBPAGE = "pawelstawicki.blogspot.com";
+    public static final String NAME_2 = "Filip";
+    public static final String SURENAME_2 = "Pająk";
+    public static final String WEBPAGE_2 = "pacykarz.blogspot.com";
 
     @Test
     public void shouldStoreUser() {
@@ -31,6 +34,24 @@ public class UserStorageTest {
         assertThat(user.getSurname()).isEqualTo(SURENAME);
         assertThat(user.getEmail()).isEqualTo(EMAIL);
         assertThat(user.getWebpage()).isEqualTo(WEBPAGE);
+    }
+
+    @Test
+    public void shouldStoreUsers() {
+        // Given
+        UserStorage storage = new UserStorage();
+
+        // When
+        storage.storeUser(new User(NAME, SURENAME, EMAIL, WEBPAGE));
+        storage.storeUser(new User(NAME_2, SURENAME_2, null, WEBPAGE_2));
+
+        // Then
+        List<User> users = storage.getUsers();
+        assertThat(users).hasSize(2);
+        assertThat(users).extracting("name").containsOnly(NAME, NAME_2);
+        assertThat(users).extracting("surname").containsOnly(SURENAME, SURENAME_2);
+        assertThat(users).extracting("email").containsOnly(EMAIL, null);
+        assertThat(users).extracting("webpage").containsOnly(WEBPAGE, WEBPAGE_2);
     }
 
 }
